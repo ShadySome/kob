e74f49955fbc2c3f40cbbacc0844e38cb2fa332a
ShadySome <1347322037@qq.com>
2023-03-21 16:05:11 +0800

    添加蛇以及游戏逻辑

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,4 +1,5 @@
 import { GameObject } from "./GameObject";
+import { Snake } from "./Snake";
 import { Wall } from "./Wall";
 
 export class GameMap extends GameObject{
@@ -9,10 +10,31 @@ export class GameMap extends GameObject{
         this.parent = parent;
         this.L = 0;
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
 
         this.walls = [];
         this.inner_wall_counts = 20;
+
+        this.snakes = [
+            new Snake({id:0,color:"#4876EC",r:this.rows-2,c:1},this),
+            new Snake({id:1,color:"#F94848",r:1,c:this.cols-2},this)
+        ];
+    }
+
+    add_listening_events()
+    {
+        this.ctx.canvas.focus();
+        const [snake0,snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown",e=>{
+            if(e.key === 'w') snake0.set_direction(0);
+            else if(e.key === 'd') snake0.set_direction(1);
+            else if(e.key === 's') snake0.set_direction(2);
+            else if(e.key === 'a') snake0.set_direction(3);
+            else if(e.key === 'ArrowUp') snake1.set_direction(0);
+            else if(e.key === 'ArrowRight') snake1.set_direction(1);
+            else if(e.key === 'ArrowDown') snake1.set_direction(2);
+            else if(e.key === 'ArrowLeft') snake1.set_direction(3);
+        });
     }
 
     start()
@@ -21,6 +43,8 @@ export class GameMap extends GameObject{
        {
             if( this.creat_walls()) break;
        }
+
+       this.add_listening_events();
     }
 
     check_connectivity(g,sx,sy,tx,ty)
@@ -50,22 +74,26 @@ export class GameMap extends GameObject{
         }
         //四周添加障碍物
         for(let r = 0; r < this.rows; r ++)
+        {
             g[r][0] = g[r][this.cols - 1] = true;
+        }
         
         for(let c = 0; c < this.cols; c ++)
-            g[0][c] = g[this.rows-1][c] = true;
+        {
+            g[0][c] = g[this.rows - 1][c] = true;
+        }
 
-        //随机创建内部障碍
-        for(let i = 0; i < this.inner_wall_counts; i ++)
+        // 随机创建内部障碍
+        for(let i = 0; i < this.inner_wall_counts / 2; i ++)
         {
             for(let j = 0; j < 1000; j ++)
             {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
-                if(g[r][c]) continue;
-                if(r == this.rows-2 && c == 1 || c == this.cols - 2 && r == 1 ) continue;
+                if(g[r][c] || g[this.rows - r - 1][this.cols - c -1]) continue;
+                if(r === this.rows-2 && c === 1 || c === this.cols - 2 && r === 1 ) continue;
 
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows - r - 1][this.cols - c - 1] = true;
                 break;
             }
         }
@@ -76,7 +104,7 @@ export class GameMap extends GameObject{
         {
             for(let c = 0; c < this.cols; c ++)
             {
-                if(g[r][c]) this.walls.push(new Wall(c,r,this));
+                if(g[r][c]) this.walls.push(new Wall(r,c,this));
             }
         }
         return true;
@@ -89,8 +117,52 @@ export class GameMap extends GameObject{
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_ready() //检查两条蛇是否都准备好下一回合了
+    {
+        for(const snake of this.snakes)
+        {
+            if(snake.status !== "idle")return false;
+            if(snake.direction === -1) return false;
+        }
+        // window.alert("ready");
+        return true;
+    }
+    //检测下一个目标单元格是否合法
+    check_valid(cell)
+    {
+        for(const wall of this.walls)
+        {
+            if(wall.r === cell.r && wall.c === cell.c) 
+                return false;
+        }
+
+        for(const snake of this.snakes)
+        {
+            let k = snake.cells.length;
+            //如果蛇尾会移动，也就是当前蛇尾这个格子会被删除，那么就不用考虑蛇尾了
+            if(!snake.check_tail_increasing())
+                k --;
+            for(let i = 0; i < k; i ++)
+            {
+                if(snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    next_step()
+    {
+        for(const snake of this.snakes)
+            snake.next_step();
+    }
+
     update(){
         this.update_size();
+        if(this.check_ready())
+        {
+            this.next_step();
+        }
         this.render();
     }
 
@@ -105,5 +177,7 @@ export class GameMap extends GameObject{
                 this.ctx.fillRect(c*this.L,r*this.L,this.L,this.L);
             }
         }
+
+
     }
 }
